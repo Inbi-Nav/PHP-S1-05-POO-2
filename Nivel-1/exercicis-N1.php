347539d0f998f32a73de4bbfe841c27a9a454e05
Inbisat Naveed <inkyn2002@gmail.com>
2025-11-05 20:57:32 +0100

    Nivel 1 Exercicse 2: implement   Shape using POO

--- Nivel-1/exercicis-N1.php
+++ Nivel-1/exercicis-N1.php
@@ -1,28 +1,25 @@
 <?php
-abstract class Animal {
 
+use LDAP\Result;
+
+abstract class Animal {
     protected $nombre;
 
     public function __construct($nombre) {
         $this->nombre = $nombre;
     }
     abstract public function hablar();
-
 }
-
 Class Pato extends Animal {
     public function hablar() {
         echo "Soy " . $this->nombre . " hago Quack Quack!<br>";
     }
 }
-
 Class Vaca extends Animal {
     public function hablar() {
         echo "Soy " . $this->nombre . " hago Muu Muu!<br>";
     }
 }
-
-
 Class Pajaro extends Animal {
     public function hablar() {
         echo "Soy " . $this->nombre . " hago Pio Pio!<br>";
@@ -37,6 +34,37 @@ echo "<br>";
 echo $Lola->hablar();
 echo "<br>";
 echo $Milly->hablar();
+?>
 
 
+<h1 style="color: darkblue;"> Exercise 2 Nivel l</h1>
+<?php
+abstract class  Shape  {
+        protected $height;
+        protected $width;
+
+        public function __construct($height, $width){
+        $this->height = $height;
+        $this->width = $width;        
+    }
+    abstract public function calculateArea();
+
+}
+class Triangle extends Shape {
+    public function calculateArea(){
+          return ($this->width * $this->height) / 2;  
+    }
+}
+class Rectangle extends Shape {
+    public function calculateArea(){
+          return ($this->width * $this->height);  
+    }
+}
+
+$Triangle = new Triangle(4, 3);
+$Rectangle = new Rectangle(18, 31);
+
+echo "Area del  triangulo es: " .  $Triangle->calculateArea();
+echo '<br />';
+echo "Area del rectanglo es: " . $Rectangle->calculateArea();
 ?>
